e16877d4bc3437b75fb6c05b277581db8ec4e148
Aurash <exiledude22@gmail.com>
2018-05-14 01:32:50 +0300

    -added the possibility of hiding the big circles; -minor fixes.

--- Circles/sketch.js
+++ Circles/sketch.js
@@ -3,33 +3,45 @@ const bigCircle = 75;
 const smallCircle = 11;
 const step = 0.07;
 
-var slider, offset, bigCircleHalf, x = 0;
+var sldFactor, cbShowCircles, offset, bigCircleHalf, x = 0,
+    showBigCircles = true;
 
 function setup() {
     createCanvas(850, 850);
-    slider = createSlider(0, 10, 6, 0.5)
+    sldFactor = createSlider(0, 10, 6, 0.5)
+    sldFactor.position(10, 870)
+    cbShowCircles = createCheckbox("Show big circles", true)
+    cbShowCircles.changed(function () {
+        showBigCircles = this.checked()
+    });
+    sldFactor.position(10, 890)
     bigCircleHalf = bigCircle / 2;
-    offset = bigCircleHalf + bigCircle % 10;
+    offset = bigCircleHalf + bigCircle * step;
 }
 
 function draw() {
     background(220);
-    
+
     for (var i = 0; i < circlesCount; i++) {
         for (var j = 0; j < circlesCount; j++) {
-            fill(0, 0, 0, 0);
-            stroke(0);
-            strokeWeight(0.5);
-            ellipse(offset * i, offset * j, bigCircle);
+            if (showBigCircles) {
+                fill(0, 0, 0, 0);
+                stroke(0);
+                strokeWeight(0.5);
+                ellipse(offset * i, offset * j, bigCircle);
+            }
 
             fill(100);
             stroke(50);
-            strokeWeight(0.1);        
-            ellipse(offset * i + bigCircleHalf * sin(x + step * (i + j) * slider.value()), 
-                offset * j + bigCircleHalf * cos(x + step * (i + j) * slider.value()),
+            strokeWeight(0.1);
+            ellipse(offset * i + bigCircleHalf * sin(x + step * (i + j) * sldFactor.value()),
+                offset * j + bigCircleHalf * cos(x + step * (i + j) * sldFactor.value()),
                 smallCircle);
         }
     }
 
     x += step;
+    if (x > 2 * PI) {
+        x = 0;
+    }
 }
